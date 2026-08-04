9280fafe0d1b1ab184d42129ddc5d0531db13fb4
Dipjoy Debnath <electrobg200@gmail.com>
2026-08-04 19:48:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.57%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def mySqrt(self, x):
+
+        left = 0
+        right = x
+        answer = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+
+            if square == x:
+                return mid
+            
+            elif square < x:
+                answer = mid
+                left = mid + 1
+
+            else:
+                right = mid - 1
+        
+        return answer
+
+        """
+        :type x: int
+        :rtype: int
+        """
+        
